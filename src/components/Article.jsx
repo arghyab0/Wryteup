02bff17010adf67931e39fd87b503290d354183e
Arghya Biswas <arghyabiswas2000@gmail.com>
2021-10-02 19:56:28 +0530

    feat: added article component

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -1,13 +1,13 @@
 const Article = () => {
   return (
     <>
-      <div className=" justify-center">
+      <div className="mx-4 my-6">
         <img
           src="https://i.pinimg.com/originals/54/ae/a9/54aea96a8a7e61acc6aabdc89ff17b2a.jpg"
           alt=""
-          className="w-64 h-36 object-cover"
+          className="w-72 h-40 object-cover"
         />
-        <div className="flex justify-center my-4">
+        <div className="flex-col items-center my-4 cursor-pointer">
           This is the title of the blog wow
         </div>
       </div>
--- src/components/Articles.jsx
+++ src/components/Articles.jsx
@@ -6,7 +6,7 @@ const Articles = () => {
     <>
       <div className="w-screen h-auto pt-16 pb-10 bg-green-400">
         <div className="flex justify-center py-8 border-2">All articles</div>
-        <div className=" justify-center pt-8 pb-14 border-2">
+        <div className=" flex flex-wrap justify-center pt-8 pb-14 px-80 border-2">
           <Article />
           <Article />
           <Article />
