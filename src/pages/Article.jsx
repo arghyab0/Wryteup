ce7b9018e54628f0b6ae0e27cba2576767677476
Arghya Biswas <arghyabiswas2000@gmail.com>
2021-10-05 22:21:57 +0530

    feat: added react md

--- src/pages/Article.jsx
+++ src/pages/Article.jsx
@@ -1,15 +1,41 @@
 //components
 import Navbar from "../components/Navbar";
+import { HiPencilAlt, HiOutlineTrash } from "react-icons/hi";
+import ReactMarkdown from "react-markdown";
 
 const Article = () => {
   return (
     <>
       <Navbar />
-      <div className="flex justify-center pb-10 border-2">
-        thsi is the title
+      <div className="flex justify-center py-10 ">
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Suscipit iste
+        fuga, nam, magnam modi enim quo
       </div>
       <div>
-        <img src="" alt="" />
+        <img
+          className="w-screen h-full object-cover"
+          src="https://resi.ze-robot.com/dl/4k/4k-desktop-wallpaper.-2560%C3%971080.jpg"
+          alt="blog cover"
+        />
+      </div>
+      <div className="mt-10 mb-14 w-1/2 mx-auto border-gray-900 border-2">
+        <div className="flex">
+          <span className="">
+            image <br /> name <br /> date
+          </span>
+          <span className="flex">
+            <HiPencilAlt className="text-2xl cursor-pointer" />
+            <HiOutlineTrash className="text-2xl cursor-pointer" />
+          </span>
+        </div>
+        <div className=" mt-14 border-gray-900 border-2 pb-64">
+          <ReactMarkdown>
+            # hello world **Lorem** ipsum dolor sit amet, consectetur
+            adipisicing elit. Temporibus explicabo, ea delectus culpa ullam
+            dolore ipsa facilis, modi laboriosam praesentium expedita aliquid in
+            deleniti rem fugiat ducimus sapiente molestiae animi.
+          </ReactMarkdown>
+        </div>
       </div>
     </>
   );
