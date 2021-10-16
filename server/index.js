3007a91ab53bf778473e23199bb38482becf60fe
Arghya Biswas <arghyabiswas2000@gmail.com>
2021-10-16 17:28:14 +0530

    feat: added conn to mongodb

--- server/index.js
+++ server/index.js
@@ -1,8 +1,16 @@
+//init server
 const express = require("express");
 const app = express();
 
-app.use("/", (req, res) => {
-  console.log("main url");
-});
+//get mongo url
+const dotenv = require("dotenv");
+dotenv.config();
+
+//mongoose connection
+const mongoose = require("mongoose");
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(console.log("Connected to MongoDB"))
+  .catch((err) => console.log(err));
 
 app.listen("5000", () => console.log("server is running at port 5000..."));
