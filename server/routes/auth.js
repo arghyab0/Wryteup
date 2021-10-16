4458ad3f719da83f6591c6b3c3f412a7c4443f5f
Arghya Biswas <arghyabiswas2000@gmail.com>
2021-10-16 18:26:25 +0530

    feat: added auth login route

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -25,5 +25,19 @@ router.post("/register", async (req, res) => {
 });
 
 //login route
+router.post("/login", async (req, res) => {
+  try {
+    const user = await User.findOne({ username: req.body.username });
+    !user && res.status(400).json("Wrong credentials");
+
+    const validated = await bcrypt.compare(req.body.password, user.password);
+    !validated && res.status(400).json("Wrong credentials");
+
+    const { password, ...rest } = user._doc;
+    res.status(200).json(rest);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 module.exports = router;
