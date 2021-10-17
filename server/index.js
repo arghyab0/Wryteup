87be91b69f9eebbd67cf853ea48fe1ab5d92286d
Arghya Biswas <arghyabiswas2000@gmail.com>
2021-10-17 12:59:37 +0530

    fix: fixed minor typo

--- server/index.js
+++ server/index.js
@@ -9,7 +9,7 @@ const mongoose = require("mongoose");
 //routes
 const authRoute = require("./routes/auth");
 const usersRoute = require("./routes/users");
-const aticlesRoute = require("./routes/articles");
+const articlesRoute = require("./routes/articles");
 
 //get mongo url
 dotenv.config();
@@ -24,6 +24,6 @@ mongoose
 
 app.use("/api/auth", authRoute);
 app.use("/api/users", usersRoute);
-app.use("/api/articles", aticlesRoute);
+app.use("/api/articles", articlesRoute);
 
 app.listen("5000", () => console.log("server is running at port 5000..."));
