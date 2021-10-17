62a4492e250a295c3c81df87e9f8a99c6fa1760e
Arghya Biswas <arghyabiswas2000@gmail.com>
2021-10-17 13:11:03 +0530

    feat: added image upload route

--- server/index.js
+++ server/index.js
@@ -5,6 +5,7 @@ const app = express();
 //imports
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
+const multer = require("multer");
 
 //routes
 const authRoute = require("./routes/auth");
@@ -14,16 +15,29 @@ const articlesRoute = require("./routes/articles");
 //get mongo url
 dotenv.config();
 
+//json middleware
 app.use(express.json());
 
 //mongoose connection
 mongoose
   .connect(process.env.MONGO_URL)
-  .then(console.log("Connected to MongoDB"))
+  .then(console.log("Connected to MongoDB."))
   .catch((err) => console.log(err));
 
+//images
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => cb(null, "images"),
+  filename: (req, file, cb) => cb(null, req.body.name),
+});
+
+const upload = multer({ storage: storage });
+app.post("/api/upload", upload.single("file"), (req, res) => {
+  res.status(200).json("File has been uploaded.");
+});
+
+//using routes
 app.use("/api/auth", authRoute);
 app.use("/api/users", usersRoute);
 app.use("/api/articles", articlesRoute);
 
-app.listen("5000", () => console.log("server is running at port 5000..."));
+app.listen("5000", () => console.log("Server running at port 5000..."));
