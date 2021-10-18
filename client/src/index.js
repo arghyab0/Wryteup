b3314714e8019408898411567a35c0f7e721c9a8
Arghya Biswas <arghyabiswas2000@gmail.com>
2021-10-18 10:54:24 +0530

    feat: set up context

--- client/src/index.js
+++ client/src/index.js
@@ -1,11 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { ContextProvider } from "./context/Context";
 import "./index.css";
 import App from "./App.jsx";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ContextProvider>
+      <App />
+    </ContextProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
