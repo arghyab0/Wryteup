3a2b24796183f45305612b8b1d92ff951c0774e1
Arghya Biswas <arghyabiswas2000@gmail.com>
2021-10-20 18:36:48 +0530

    styling: footer comp styling

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -6,13 +6,25 @@ const Footer = () => {
           <div className="font-heading text-5xl my-8">WRYTEUP</div>
           <div className="font-ui py-4">A build by Arghya Biswas.</div>
           <div className="w-1/2 mx-auto my-8 flex justify-around">
-            <a href="https://arghyabiswas.me/" target="blank">
+            <a
+              className="border-b"
+              href="https://arghyabiswas.me/"
+              target="blank"
+            >
               Website
             </a>
-            <a href="https://twitter.com/arghyab0" target="blank">
+            <a
+              className="border-b"
+              href="https://twitter.com/arghyab0"
+              target="blank"
+            >
               Twitter
             </a>
-            <a href="https://github.com/arghyab0/Wryteup" target="blank">
+            <a
+              className="border-b"
+              href="https://github.com/arghyab0/Wryteup"
+              target="blank"
+            >
               Github
             </a>
           </div>
