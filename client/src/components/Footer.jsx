659777bc7b110919d331b8348a2410f4dbb71c0c
Arghya Biswas <arghyabiswas2000@gmail.com>
2021-10-22 19:09:44 +0530

    style: responsive footer

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,11 +1,16 @@
 const Footer = () => {
   return (
     <>
-      <div className="w-screen pt-14 pb-10 bg-black">
-        <div className="w-1/2 mx-auto text-white text-center">
-          <div className="font-heading text-5xl my-8">WRYTEUP</div>
-          <div className="font-ui py-4">A build by Arghya Biswas.</div>
-          <div className="w-1/2 mx-auto my-8 flex justify-around">
+      <div className="w-screen pt-6 md:pt-14 pb-4 md:pb-10 bg-black">
+        <div className="w-4/5 md:w-1/2 mx-auto text-white text-center">
+          <div className="font-heading text-3xl md:text-5xl my-8">WRYTEUP</div>
+          <div className="text-sm md:text-base font-ui py-2 md:py-4">
+            A build by Arghya Biswas. Lorem ipsum dolor, sit amet consectetur
+            adipisicing elit. Necessitatibus ad beatae, fugit animi explicabo
+            amet exercitationem deserunt voluptate quaerat soluta totam, nemo
+            illo. Quisquam, neque quae officia illo ut dolorem.
+          </div>
+          <div className="w-4/5 md:w-1/2 mx-auto my-4 md:my-8 flex justify-around">
             <a
               className="border-b"
               href="https://arghyabiswas.me/"
