a5e0a94471f3a05ac9dd14400ac698865804344a
Arghya Biswas <arghyabiswas2000@gmail.com>
2021-10-22 19:22:04 +0530

    style: responsive register comp

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -26,14 +26,16 @@ const Register = () => {
 
   return (
     <>
-      <div className="w-1/2 mx-auto text-center">
-        <h1 className="font-heading text-5xl py-10">Register</h1>
+      <div className="mx-2 md:w-1/2 md:mx-auto text-center">
+        <h1 className="font-heading text-4xl md:text-5xl py-6 md:py-10">
+          Register
+        </h1>
         <form onSubmit={handleSubmit} className="font-ui text-lg ">
           <div className="pb-6">
             <label>Username</label>
             <input
               type="text"
-              className="mx-8 p-1 border-2 rounded-lg border-gray-400 "
+              className="mx-2 md:mx-8 p-1 border-2 rounded-lg border-gray-400 "
               placeholder="username"
               onChange={(e) => setUsername(e.target.value)}
             />
@@ -43,7 +45,7 @@ const Register = () => {
             <label>Email</label>
             <input
               type="email"
-              className="ml-10 p-1 border-2 rounded-lg border-gray-400"
+              className="mx-2 md:mx-8 p-1 border-2 rounded-lg border-gray-400"
               placeholder="email"
               onChange={(e) => setEmail(e.target.value)}
             />
@@ -53,7 +55,7 @@ const Register = () => {
             <label>Password</label>
             <input
               type="password"
-              className="mx-8 p-1 border-2 rounded-lg border-gray-400"
+              className="mx-2 md:mx-8 p-1 border-2 rounded-lg border-gray-400"
               placeholder="password"
               onChange={(e) => setPassword(e.target.value)}
             />
