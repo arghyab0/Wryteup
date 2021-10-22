c71ce6dca0107eae13b5f95ab1d7ccfb8aec7e9b
Arghya Biswas <arghyabiswas2000@gmail.com>
2021-10-22 21:07:10 +0530

    fix: deployment

--- index.js
+++ index.js
@@ -42,12 +42,12 @@ app.use("/api/auth", authRoute);
 app.use("/api/users", usersRoute);
 app.use("/api/articles", articlesRoute);
 
-// if (process.env.NODE.ENV === "production") {
-app.use(express.static(path.resolve(__dirname, "./client/build")));
-app.get("*", function (request, response) {
-  response.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
-});
-// }
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.resolve(__dirname, "./client/build")));
+  app.get("*", function (req, res) {
+    res.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
+  });
+}
 
 const port = process.env.PORT || 3080;
 
